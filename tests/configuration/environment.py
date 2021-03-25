c8aa8371bfb6739fbb4b281527bb16d947f1eea4
Laurence Pakenham-Smith <laurence.smith@airelogic.com>
2021-03-25 10:27:42 +0000

    APM-1973 First cut e2e test

--- tests/configuration/environment.py
+++ tests/configuration/environment.py
@@ -12,8 +12,12 @@ def get_env(variable_name: str) -> str:
         raise RuntimeError(f"Variable is not set, Check {variable_name}.")
 
 
+oauth_proxy = get_env("OAUTH_PROXY")
+oauth_base_uri = get_env("OAUTH_BASE_URI")
+
 ENV = {
-    # Apigee
-    "environment": get_env("APIGEE_ENVIRONMENT"),
-    "base_path": get_env("SERVICE_BASE_PATH"),
+    "product": get_env("APIGEE_PRODUCT"),
+    "oauth_proxy": oauth_proxy,
+    "oauth_base_uri": oauth_base_uri,
+    "token_url": f"{oauth_base_uri}/{oauth_proxy}/token"
 }
