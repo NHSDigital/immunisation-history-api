60cb3a1df8e29dac18fe3d02330f6a5805496ed0
Laurence Pakenham-Smith <laurence.smith@airelogic.com>
2021-06-17 08:08:03 +0100

    DALA-302 Check for `null` as well

--- proxies/live/apiproxy/resources/jsc/SetDefaultAllowedProofingLevel.js
+++ proxies/live/apiproxy/resources/jsc/SetDefaultAllowedProofingLevel.js
@@ -1,5 +1,5 @@
 var allowedProofingLevel = context.getVariable('developer.app.nhs-login-allowed-proofing-level');
 
-if (allowedProofingLevel === undefined || allowedProofingLevel === "") {
+if (allowedProofingLevel === undefined || allowedProofingLevel === "" || allowedProofingLevel === null) {
     context.setVariable('developer.app.nhs-login-allowed-proofing-level', 'P9');
 }
