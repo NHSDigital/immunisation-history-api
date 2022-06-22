c2b1524df1406c4c26cb35fe70c2ed08f065925b
Tim Wang <tianxun.wang1@nhs.net>
2022-06-22 16:48:44 +0100

    DALA-930 added more cors headers

--- sandbox/app.js
+++ sandbox/app.js
@@ -126,7 +126,18 @@ function on_error(err, req, res, next) {
 const handlers = require('./handlers');
 app.use(
   cors({
-    origin: 'https://digital.nhs.uk'
+    origin: 'https://digital.nhs.uk',
+    allowedHeaders: [
+      'origin',
+      'x-requested-with',
+      'accept',
+      'content-type',
+      'nhsd-session-urid',
+      'nhsd-user-identity',
+      'x-correlation-id'
+    ],
+    maxAge: 3628800,
+    methods: ['GET', 'PUT', 'POST', 'DELETE']
   })
 );
 app.use(before_request);
