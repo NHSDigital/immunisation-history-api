c4591932a4177e1be0d71470ad9c24d62e507d35
Joel Klinger <joel.klinger1@nhs.net>
2022-06-27 15:37:23 +0100

    [DALA-972] Set strict authorised targets

---
+++ proxies/live/apiproxy/resources/jsc/AuthorisedTargets.SetStrict.js
@@ -0,0 +1,4 @@
+var inProdEnvironment = context.getVariable('environment.name') === "prod";
+var useStrictAuthorisedTargets = context.getVariable('app.use_strict_authorised_targets') === true;
+
+context.setVariable('apigee.USE_STRICT_AUTHORISED_TARGETS', useStrictAuthorisedTargets || inProdEnvironment);
