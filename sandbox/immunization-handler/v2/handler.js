e7e86149354b660231904ac77ae3dfd05ebf0bc5
Jim Alcock <james.alcock1@nhs.net>
2023-05-03 11:43:56 +0100

    added Flu response to api

--- sandbox/immunization-handler/v2/handler.js
+++ sandbox/immunization-handler/v2/handler.js
@@ -2,6 +2,7 @@ const { emptyImmunizationFhir } = require('./fhir-responses/empty-immunization.f
 const { SNOMED_PROCEDURE_CODES, IMMUNIZATION_TARGETS } = require('./constants');
 const { covidImmunizationFhir } = require('./fhir-responses/covid-immunization.fhir');
 const { hpvImmunizationFhir } = require('./fhir-responses/hpv-immunization.fhir');
+const { fluImmunizationFhir } = require('./fhir-responses/flu-immunization.fhir')
 const { parseDateRange, validateDateRange } = require('./date-range');
 const { writeLog } = require('../../logging');
 const { HTTP_STATUS, API_VERSIONS } = require('../constants');
@@ -30,6 +31,11 @@ function getImmunizationResponse(
   if (immunizationTarget === IMMUNIZATION_TARGETS.HPV) {
     return hpvImmunizationFhir(dateFrom, dateTo);
   }
+
+  if (fluImmunizationFhir === IMMUNIZATION_TARGETS.FLU) {
+    return fluImmunizationFhir(dateFrom, dateTo);
+  }
+
 }
 
 function getFhirResponse(
